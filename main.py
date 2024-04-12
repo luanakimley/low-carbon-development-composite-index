c5492873ccdfb7e0dc54bdcd19dd35dcf45d4e67
luanakimley <luanaa.kimley@gmail.com>
2024-04-12 15:01:37 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.stats import pearsonr
+import statsmodels.api as sm
+import pandas as pd
+
+# Load data
+
+# Inputation of missing values
+
+# Multivariate analysis
+
+# Normalisation
+
+# Weighting and aggregation
